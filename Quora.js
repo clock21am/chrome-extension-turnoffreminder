32600e4584ac574e72eae628769e5a6f0683ebd5
Rajdeep Kaur <rajdeep51994@gmail.com>
2016-11-25 06:37:21 +0530

    content script modified

---
+++ Quora.js
@@ -0,0 +1,5 @@
+//window.setTimeout(myfunction,1000);
+alert("Quora");
+/*chrome.runtime.onMessage.addListener(function(R,S,A) {
+	alert(R);
+});*/
---
+++ facebook.js
@@ -0,0 +1,4 @@
+window.setTimeout(myfunction,1000);
+/*chrome.runtime.onMessage.addListener(function(R,S,A) {
+	alert(R);
+});*/
