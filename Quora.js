8e5400587ebccb6ae98a90517a33c3a9bc74e0b9
opentechlab <shruti.nitdgp@gmail.com>
2017-03-26 22:55:28 +0530

    Update Quora.js

--- Quora.js
+++ Quora.js
@@ -1,5 +1,33 @@
-var timer = window.setInterval(logout, 6000);
 
+
+var isActive;
+var timer;
+window.onblur = function () { 
+  isActive = true; 
+clearInterval(timer);
+   timer = window.setInterval(logout, 6000);
+
+}; 
+
+
+window.onfocus = function () { 
+  isActive = true; 
+clearInterval(timer);
+
+ timer = window.setInterval(logout, 600000);
+
+
+document.onmousemove = function() {
+clearInterval(timer);
+   timer = window.setInterval(logout, 60000);
+}
+
+document.onmouseout = function() {
+clearInterval(timer);
+   timer = window.setInterval(logout, 6000);
+}
+
+};
 function logout() {
 	var toggledownbutton = document.getElementsByClassName("selector_input text")[0];
 	if(toggledownbutton==null){
